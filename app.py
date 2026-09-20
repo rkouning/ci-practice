eda0ec5b578a49878a72ac85e1beb478fb6bff26
Роман Кокунин <romankokunin@Romans-MacBook-Air.local>
2026-09-20 15:16:14 +0300

    Add Docker build stage

---
+++ app.py
@@ -0,0 +1,10 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(b"Privet iz konteynera!\n")
+
+HTTPServer(('0.0.0.0', 8000), Handler).serve_forever()
